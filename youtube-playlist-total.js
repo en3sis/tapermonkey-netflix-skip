6acacc78562a654dc937ce18042eb26a21741127
Sorin Curescu <scurescu@suse.com>
2021-02-07 20:15:07 +0100

    Update youtube-playlist-total.js

--- youtube-playlist-total.js
+++ youtube-playlist-total.js
@@ -14,44 +14,29 @@
   'use strict';
 
   const getTotalTime = () => {
-    const regex = /(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)(?:[000-999])/gm
-    let labels = Array.from(document.querySelectorAll('.ytd-thumbnail-overlay-time-status-renderer')).filter(ele => ele.textContent)
 
-    const splitItems = labels.map(item => {
-      return item.textContent.match(regex)
-    }).map(ele => ele[0].split(':'))
+    let labels = [...document.querySelectorAll("ytd-thumbnail-overlay-time-status-renderer > span")]
 
-    let min = 0
-    let sec = 0
-
-    splitItems.forEach(item => {
-      min = min + parseInt(item[0])
-      sec = sec + parseInt(item[1])
-    })
-
-    const totalMin = Math.floor(min + Math.min(sec) / 60)
-
-    function timeConvert (n) {
-      const num = n;
-      const rhours = Math.floor((num / 60))
-      const hours = (num / 60);
-      const rminutes = Math.round((hours - rhours) * 60);
-      return `Playlist total: ${rhours}:${rminutes}:${Math.round(sec > 60 ? sec / 60 : sec)}`
-    }
-
-    console.log(timeConvert(totalMin));
+    labels
+    .reduce((acc, dur) => {
+        dur = dur.textContent.trim().split(':');
+        return acc + parseInt(dur[0]) * 60 + parseInt(dur[1]);
+      }, 0) / 60;
 
     const div = document.createElement('div');
-    div.style.cssText = `position: fixed;
+    div.style.cssText = `osition: fixed;
 z-index: 999;
 width: auto;
 bottom: 10px;
 right: 10px;
-background-color: black;
-color: #fff;
-padding: 20px;
-font-size: 14px;`;
-    div.innerHTML = timeConvert(totalMin);
+background-color: #f00;
+color: rgb(255, 255, 255);
+padding: 15px;
+font-size: 14px;
+border-radius: 50px;
+font-weight: bold;
+border: 3px solid #9e1212;`;
+    div.innerHTML = `${Math.floor((labels / 60) + " hours " + minutes % 60 + " minutes.")}`;
 
     return document.body.appendChild(div);
   }
